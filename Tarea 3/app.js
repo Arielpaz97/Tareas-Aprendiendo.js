79805971ad48025d789b6169e9432b9009e1699d
arielpaz97 <arielpaz97@hotmail.com>
2020-07-09 16:57:53 -0300

    funciona!

--- Tarea 3/app.js
+++ Tarea 3/app.js
@@ -1,39 +1,26 @@
 const fs = require('fs')
 
-const segurosDeAutos = async (marca) => ({
+const segurosDeAutos = (marca) => ({
   "Renault": 0.8,
   "Ford": 1,
   "Chevrolet": 0.5
 })[marca]
 
-(async _ => {
-  let autos = []
-  try {
-    const stringObjeto = fs.readFileSync('./autos.json')
-    autos = JSON.parse(stringObjeto)
-    const newAutos = await getNewAutos(autos)
-    const objetoString = JSON.stringify(newAutos, null, 4)
-    fs.writeFileSync('./autos.json', objetoString)
-  } catch (error) {
-    console.log('Tu JSON no es válido')
-  } 
-})()
-
-const calcularSeguro = async (auto) => {
+const calcularSeguro = (auto) => {
   let vaAPagar = 1000
   const porcentaje = segurosDeAutos(auto.marca)
   vaAPagar *= porcentaje
   return vaAPagar
 }
 
-const getNewAutos = async (autos) => {
+const getNewSeguros = (autos) => {
   let totalAPagar = 0
   if (autos.length) {
     let i
     for (i = 0; i < autos.length; i++) {
       const auto = autos[i]
       if (auto.marca && auto.modelo) {
-        const seguro = await calcularSeguro(auto)
+        const seguro = calcularSeguro(auto)
         console.log(`El seguro de ${auto.marca} ${auto.modelo} es:$`, seguro)
         auto.seguro = seguro
         totalAPagar += seguro
@@ -44,7 +31,22 @@ const getNewAutos = async (autos) => {
       }
     }
     console.log(`El Total a pagar es de:$${totalAPagar}`)
+    return autos
   } else {
     console.log('No hay autos a calcular')
+    return autos
   }
 }
+
+(async _ => {
+  let autos = []
+  try {
+    const stringObjeto = fs.readFileSync('./autos.json')
+    autos = JSON.parse(stringObjeto)
+    const newAutos = getNewSeguros(autos)
+    const objetoString = JSON.stringify(newAutos, null, 4)
+    fs.writeFileSync('./autos.json', objetoString)
+  } catch (error) {
+    console.log(error)
+  }
+})()
